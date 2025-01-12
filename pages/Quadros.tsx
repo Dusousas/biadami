01a02cc26410f1069a95ca3e71663c28db4e641d
Dusousas <dusousadeveloper@gmail.com>
2025-01-12 16:31:24 -0300

    att quadros

--- pages/Quadros.tsx
+++ pages/Quadros.tsx
@@ -6,37 +6,59 @@ export default function Quadros() {
             <section className='py-8'>
                 <div className=' mx-auto px-4'>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-3 gap-4">
-                        <div className=''>
-                            <img className='w-full h-full object-cover' src="quadros/img-5.jpg" alt="" />
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-3 gap-4 ">
+
+                        <div className='border-black border-[10px]'>
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/img-5.jpg" alt="" />
+                            </div>
                         </div>
 
-                        <div className="row-span-2 col-span-1 sm:col-span-1 lg:col-start-1 lg:row-start-2">
-                            <img className='w-full h-full object-cover' src="quadros/Portfolio-2.jpg" alt="" />
+                        <div className="row-span-2 col-span-1 sm:col-span-1 lg:col-start-1 lg:row-start-2 border-black border-[10px]">
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/Portfolio-2.jpg" alt="" />
+                            </div>
                         </div>
 
-                        <div className="row-span-2 col-span-1 sm:col-span-1 lg:col-start-2 lg:row-start-1">
-                            <img className='w-full h-full object-cover' src="quadros/Portfolio-2.jpg" alt="" />
+                        <div className="row-span-2 col-span-1 sm:col-span-1 lg:col-start-2 lg:row-start-1 border-black border-[10px]">
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/Portfolio-2.jpg" alt="" />
+                            </div>
                         </div>
 
-                        <div className="row-span-1 col-span-1 sm:col-span-1 lg:col-start-2 lg:row-start-3">
-                            <img className='w-full h-full object-cover' src="quadros/Portfolio-5.jpg" alt="" />
+                        <div className="row-span-1 col-span-1 sm:col-span-1 lg:col-start-2 lg:row-start-3 border-black border-[10px]">
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/Portfolio-5.jpg" alt="" />
+
+                            </div>
                         </div>
 
-                        <div className="col-span-1 sm:col-span-1 lg:col-start-3 lg:row-start-1">
-                            <img className='w-full h-full object-cover' src="quadros/Portfolio-3.jpg" alt="" />
+                        <div className="col-span-1 sm:col-span-1 lg:col-start-3 lg:row-start-1 border-black border-[10px]">
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/Portfolio-3.jpg" alt="" />
+
+                            </div>
                         </div>
 
-                        <div className="col-span-1 sm:col-span-1 lg:col-start-3 lg:row-start-2">
-                            <img className='w-full h-full object-cover' src="quadros/img-5.jpg" alt="" />
+                        <div className="col-span-1 sm:col-span-1 lg:col-start-3 lg:row-start-2 border-black border-[10px]">
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/img-5.jpg" alt="" />
+
+                            </div>
                         </div>
 
-                        <div className="col-span-1 sm:col-span-1 lg:col-start-3 lg:row-start-3">
-                            <img className='w-full h-full object-cover' src="quadros/Portfolio-3.jpg" alt="" />
+                        <div className="col-span-1 sm:col-span-1 lg:col-start-3 lg:row-start-3 border-black border-[10px]">
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/Portfolio-3.jpg" alt="" />
+
+                            </div>
                         </div>
 
-                        <div className="row-span-3 col-span-1 sm:col-span-1 lg:col-start-4 lg:row-start-1">
-                            <img className='w-full h-full object-cover' src="quadros/Portfolio-4.jpg" alt="" />
+                        <div className="row-span-3 col-span-1 sm:col-span-1 lg:col-start-4 lg:row-start-1 border-black border-[10px]">
+
+                            <div className='w-full h-full p-4'>
+                                <img className='w-full h-full object-cover' src="quadros/Portfolio-4.jpg" alt="" />
+                            </div>
                         </div>
                     </div>
 
